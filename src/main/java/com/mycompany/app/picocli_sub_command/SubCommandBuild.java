c60b6c6f5b2b4759776fea916e901796e694c31b
Klab93 <37708074+Klab93@users.noreply.github.com>
2021-03-09 10:16:13 +0100

    build sub-command++

--- src/main/java/com/mycompany/app/picocli_sub_command/SubCommandBuild.java
+++ src/main/java/com/mycompany/app/picocli_sub_command/SubCommandBuild.java
@@ -1,12 +1,12 @@
 package com.mycompany.app.picocli_sub_command;
 
 import picocli.CommandLine.Command;
-
 import java.util.concurrent.Callable;
 
 @Command(name = "build", description = "build command")
-public class Build implements Callable<Integer> {
+public class SubCommandBuild implements Callable<Integer> {
 
+    @Override
     public Integer call() throws Exception {
         System.out.println("Buid static website");
         return 0;
