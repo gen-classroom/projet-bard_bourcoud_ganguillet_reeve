72676145071f6c43849921ebe44fef90e09e41b5
PaulReeve717 <paulreeve717@gmail.com>
2021-03-18 13:16:37 +0100

    Début de clean

--- src/main/java/com/mycompany/app/picocli_sub_command/SubCommandClean.java
+++ src/main/java/com/mycompany/app/picocli_sub_command/SubCommandClean.java
@@ -1,5 +1,7 @@
 package com.mycompany.app.picocli_sub_command;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.Callable;
 import picocli.CommandLine.Command;
 
@@ -8,7 +10,20 @@ public class SubCommandClean implements Callable<Integer> {
 
     @Override
     public Integer call() {
-        System.out.println("Clean.");
+            String currentPath = System.getProperty("user.dir");
+            File buildDirectory = new File(currentPath + "/build");
+            deleteDirectory(buildDirectory);
         return 0;
+    }
+
+    private boolean deleteDirectory(File directory)
+    {
+        File[] allContents = directory.listFiles();
+        if (allContents != null) {
+            for (File file : allContents) {
+                deleteDirectory(file);
+            }
+        }
+        return directory.delete();
     }
 }
