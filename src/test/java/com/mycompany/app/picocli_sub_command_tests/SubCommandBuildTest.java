9b23cc152743329f2ea2fbd3daa76d2a2998b265
soso24soso <61196412+soso24soso@users.noreply.github.com>
2021-03-25 12:41:41 +0100

    rajout test provisoire

--- src/test/java/com/mycompany/app/picocli_sub_command_tests/SubCommandBuildTest.java
+++ src/test/java/com/mycompany/app/picocli_sub_command_tests/SubCommandBuildTest.java
@@ -1,4 +1,16 @@
 package com.mycompany.app.picocli_sub_command_tests;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
 public class SubCommandBuildTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
 }
