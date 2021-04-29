dc0867738b2370a3e1cfd27cbea2acf23a36755c
Klab93 <37708074+Klab93@users.noreply.github.com>
2021-04-29 12:43:08 +0200

    teste création des fichiers++

--- src/test/java/com/mycompany/app/picocli_sub_command_tests/SubCommandInitTest.java
+++ src/test/java/com/mycompany/app/picocli_sub_command_tests/SubCommandInitTest.java
@@ -1,6 +1,5 @@
 package com.mycompany.app.picocli_sub_command_tests;
 
-
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 
@@ -11,17 +10,19 @@ import picocli.CommandLine;
 import static org.junit.Assert.assertTrue;
 
 public class SubCommandInitTest {
-    private final ByteArrayOutputStream output = new ByteArrayOutputStream();
 
     @Test
-    void fileAreCreatedWithInitCommand() throws Exception {
-        new CommandLine(new Main()).execute("init /test");
+    public void fileAreCreatedWithInitCommand() throws Exception {
+        new CommandLine(new Main()).execute("init", "/test");
         StringBuilder path = new StringBuilder("./test");
+
         assertTrue( new File(path.toString()).exists() );
         assertTrue( new File(path.toString() + "/index.md").exists() );
         assertTrue( new File(path.toString() + "/config.json").exists() );
         assertTrue( new File(path.toString() + "/contents").exists() );
+
         path.append("/template");
+
         assertTrue( new File(path.toString()).exists() );
         assertTrue( new File(path.toString() + "/menu.html").exists() );
         assertTrue( new File(path.toString() + "/layout.html").exists() );
