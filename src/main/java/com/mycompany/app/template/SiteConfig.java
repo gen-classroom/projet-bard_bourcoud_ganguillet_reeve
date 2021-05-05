00f23b163a3b34f2c6e6aaa2b280672e40dc3769
soso24soso <61196412+soso24soso@users.noreply.github.com>
2021-05-05 17:33:13 +0200

    site config fini

--- src/main/java/com/mycompany/app/template/SiteConfig.java
+++ src/main/java/com/mycompany/app/template/SiteConfig.java
@@ -1,5 +1,32 @@
 package com.mycompany.app.template;
 
-public class SiteConfig {
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
+import java.io.FileReader;
+import java.io.IOException;
+
+public class SiteConfig {
+    private String domain;
+    private String title;
+
+    public String getDomain() {
+        return domain;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public SiteConfig(String configFile){
+        JSONParser parser = new JSONParser();
+        try (FileReader fileReader = new FileReader(configFile)){
+            JSONObject o = (JSONObject) parser.parse(fileReader);
+            domain = (String) o.get("domain");
+            title = (String) o.get("title");
+        } catch(IOException | ParseException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
