55112a70c813cdf97d34e59338279bfeff2d1ebc
soso24soso <61196412+soso24soso@users.noreply.github.com>
2021-05-06 22:14:27 +0200

    test subCommandBuild Fait

--- src/test/java/com/mycompany/app/picocli_sub_command_tests/SubCommandBuildTest.java
+++ src/test/java/com/mycompany/app/picocli_sub_command_tests/SubCommandBuildTest.java
@@ -4,12 +4,20 @@ import com.mycompany.app.Main;
 import org.junit.Test;
 import picocli.CommandLine;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class SubCommandBuildTest {
 
     @Test
-    public void shouldCreateSubDirectoryBuild() {
+    public void shouldCreateSubDirectoryBuild() throws FileNotFoundException {
+        new SubCommandInitTest().fileAreCreatedWithInitCommand();
+
         int returnValue = new CommandLine(new Main()).execute("build", "/mySite");
 
         assertEquals(returnValue, 0);
