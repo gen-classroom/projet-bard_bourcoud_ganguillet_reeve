3cc65b20deb578681ef28770baf8efa51f645e5c
soso24soso <61196412+soso24soso@users.noreply.github.com>
2021-05-06 11:36:11 +0200

    rajout de la doc

--- src/main/java/com/mycompany/app/template/PageMetaData.java
+++ src/main/java/com/mycompany/app/template/PageMetaData.java
@@ -1,14 +1,35 @@
 package com.mycompany.app.template;
 
+/**
+ * Classe représentant les métadonnées associées à une page.
+ */
 public class PageMetaData {
+    /**
+     * Crée un objet contenant les métadonnées.
+     *
+     * @param title  Le titre de la page.
+     * @param author Le nom de l'auteur de la page.
+     * @param date   Le date de publication de la page.
+     */
     public PageMetaData(String title, String author, String date) {
         this.title = title;
         this.author = author;
         this.date = date;
     }
 
+    /**
+     * Le titre de la page.
+     */
     private final String title;
+
+    /**
+     * Le nom de l'auteur de la page.
+     */
     private final String author;
+
+    /**
+     * La date de publication de la page.
+     */
     private final String date;
 
     /**
