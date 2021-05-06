14598e0f1923a1aba3a8b596becf42ff6eb41f61
soso24soso <61196412+soso24soso@users.noreply.github.com>
2021-05-06 11:24:45 +0200

    adaptation des test pour MDPageParser

--- src/test/java/com/mycompany/app/PageTest.java
+++ src/test/java/com/mycompany/app/PageTest.java
@@ -1,13 +1,14 @@
 package com.mycompany.app;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
-import java.io.IOException;
-import java.io.StringReader;
+import java.io.*;
 
 import com.mycompany.app.template.MDPageParser;
+import com.mycompany.app.template.PageData;
+import com.mycompany.app.template.PageMetaData;
+import com.mycompany.app.template.SiteConfig;
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 /**
  * Les tests unitaires pour la classe Page.
@@ -23,22 +24,21 @@ public class PageTest {
         StringReader exampleFromBacklog = new StringReader("titre: Mon premier article\n" + "auteur: Bertil Chapuis\n"
                 + "date: 2021-03-10\n" + "---\n" + "# Mon premier article\n" + "\n" + "## Mon sous-titre\n" + "\n"
                 + "Le contenu de mon article.\n" + "\n" + "![Une image](./image.png)");
-        MDPageParser page;
-        /*
+        MDPageParser page = new MDPageParser(new SiteConfig("./mySite/config.json"));
+
         try {
-
-            page = new MDPageParser(exampleFromBacklog);
-
-            assertEquals(page.getTitle(), "Mon premier article");
-            assertEquals(page.getAuthor(), "Bertil Chapuis");
-            assertEquals(page.getDate(), "2021-03-10");
-            assertEquals(page.getContentAsHtml(), "<h1>Mon premier article</h1>\n" + "<h2>Mon sous-titre</h2>\n"
+            File f = new File("./mySite/file.md");
+            PageData pageData = page.parse(f);
+            PageMetaData metaData = pageData.getPage();
+            assertEquals(metaData.getTitle(), "Mon premier article");
+            assertEquals(metaData.getAuthor(), "Bertil Chapuis");
+            assertEquals(metaData.getDate(), "2021-03-10");
+            assertEquals(pageData.getContent(), "<h1>Mon premier article</h1>\n" + "<h2>Mon sous-titre</h2>\n"
                     + "<p>Le contenu de mon article.</p>\n" + "<p><img src=\"./image.png\" alt=\"Une image\" /></p>\n");
 
-        } catch (IOException e) {
-            e.printStackTrace();
-            assertTrue(false);
+        } catch(IOException e) {
+            System.out.println(e.getMessage());
+            fail();
         }
-        */
     }
 }
