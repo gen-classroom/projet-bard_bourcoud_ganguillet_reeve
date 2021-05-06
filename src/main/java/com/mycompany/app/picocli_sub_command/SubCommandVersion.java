1fc68c9b434ad27eead89198ff2973af9c3e4f72
Klab93 <37708074+Klab93@users.noreply.github.com>
2021-05-06 12:05:30 +0200

    version

--- src/main/java/com/mycompany/app/picocli_sub_command/SubCommandVersion.java
+++ src/main/java/com/mycompany/app/picocli_sub_command/SubCommandVersion.java
@@ -1,7 +1,11 @@
 package com.mycompany.app.picocli_sub_command;
 
+import com.mycompany.app.Main;
 import picocli.CommandLine;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.Callable;
 
 @CommandLine.Command(name = "-version", description = "Give the version of the programm.")
@@ -9,7 +13,20 @@ public class SubCommandVersion implements Callable<Integer> {
 
     @Override
     public Integer call() {
-        System.out.println("version : 0.0.1");
+        /*
+        Package mainPackage =Main.class.getPackage();
+        String version = mainPackage.getImplementationVersion();
+        System.out.println("version : " + version);
+        */
+
+        try {
+            Properties appProps = new Properties();
+            appProps.load(getClass().getClassLoader().getResourceAsStream("config.properties"));
+            System.out.println("version : " + appProps.getProperty("version"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         return 0;
     }
 }
