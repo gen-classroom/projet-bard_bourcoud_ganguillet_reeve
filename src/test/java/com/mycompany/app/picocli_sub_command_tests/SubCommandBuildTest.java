24b3b32aaf74cc196f7e3124d506f9ecbb39666e
arbroween <arbroween@gmail.com>
2021-06-02 17:49:56 +0200

    corrigé bug pour passer test

--- src/test/java/com/mycompany/app/picocli_sub_command_tests/SubCommandBuildTest.java
+++ src/test/java/com/mycompany/app/picocli_sub_command_tests/SubCommandBuildTest.java
@@ -28,22 +28,13 @@ public class SubCommandBuildTest {
         assertTrue(indexHtml.exists());
 
         BufferedReader indexReader = new BufferedReader(new FileReader(indexHtml));
-        String indexContent =
-                indexReader.lines()
-                        .reduce("", (String acc, String line) -> acc + "\n" + line)
-                        .trim();
-        assertEquals(indexContent, "<html lang=\"en\">\n" +
-                "<head>\n" +
-                "\t<meta charset=\"utf-8\">\n" +
-                "\t<title>mon Site | Mon premier article</title>\n" +
-                "</head>\n" +
-                "<body>\n" +
-                "\t<ul>\n" +
-                "\t<li><a href=\"/index.html\">home</a></li>\n" +
-                "\t<li><a href=\"/content/page.html\">page</a></li>\n" +
-                "</ul>\n" +
-                "\t\n" +
-                "</body>\n" +
-                "</html>");
+        String indexContent = indexReader.lines().reduce("", (String acc, String line) -> acc + "\n" + line).trim();
+        assertEquals(indexContent,
+                "<html lang=\"en\">\n" + "<head>\n" + "\t<meta charset=\"utf-8\">\n"
+                        + "\t<title>mon Site | Mon premier article</title>\n" + "</head>\n" + "<body>\n" + "\t<ul>\n"
+                        + "\t<li><a href=\"/index.html\">home</a></li>\n"
+                        + "\t<li><a href=\"/content/page.html\">page</a></li>\n" + "</ul>\n"
+                        + "\t<h1>Mon premier article</h1>\n" + "<h2>Mon sous-titre</h2>\n"
+                        + "<p>Le contenu de mon article.</p>\n" + "\n" + "</body>\n" + "</html>");
     }
 }
